1c3d1d1e2dca8253cc34a3424110fff48c125a88
Henry Tran <h98.tran@gmail.com>
2020-10-07 11:33:57 -0400

    Updates

---
+++ precache-manifest.bda1807c4c4df5c1178ef031fce8f5e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1655a78ec8f210d2310cac1a5498aa94",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "be0958711e173bdb96d3",
+    "url": "/Currency-Converter/static/css/main.9ecca3b8.chunk.css"
+  },
+  {
+    "revision": "c733cf7963be1c15d7dd",
+    "url": "/Currency-Converter/static/js/2.6b66c650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-Converter/static/js/2.6b66c650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0958711e173bdb96d3",
+    "url": "/Currency-Converter/static/js/main.7a85906b.chunk.js"
+  },
+  {
+    "revision": "e7cd008e12b73d955271",
+    "url": "/Currency-Converter/static/js/runtime-main.0ec7fc0c.js"
+  }
+]);
